b66436475cdb246259df60132a4a65bf0dfff48d
mustafa <yafess229@gmail.com>
2022-05-31 14:26:22 +0300

    Update

---
+++ Js-questions/To square(root) or not to square(root).js
@@ -0,0 +1,21 @@
+
+/*Return a new array with processing every number of the input-array like this:
+
+If the number has an integer square root, take this, otherwise square the number.
+
+Example
+[4,3,9,7,2,1] -> [2,9,3,49,4,1]
+Notes
+The input array will always contain only positive numbers, and will never be empty or null. */
+
+
+//solution-1
+
+function squareOrSquareRoot(array) {
+  
+    return array.map(item => Math.sqrt(item) == Math.sqrt(item).toFixed(0) ?  Math.sqrt(item) : item * item);
+  
+    
+  }
+
+
