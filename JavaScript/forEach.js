47d8478dedc0d5f46d06afb4f6ec5c3c17f0fd33
mustafa <yafess229@gmail.com>
2022-06-02 17:14:52 +0300

    operation is done

---
+++ JavaScript/forEach.js
@@ -0,0 +1,31 @@
+//ForEach
+
+
+// const number = [1,2,3,4,5];
+
+// number.forEach((number,index,array) =>{
+
+//     console.log(number,index);
+// })
+
+
+const users = [
+    {
+        name : "Emir",
+        age :19
+    },
+    {
+        name : "Mustafa",
+        age : 29
+    },
+    {
+        name : "Ayse",
+        age : 42
+    }
+];
+
+users.forEach(v =>{
+    console.log(v.name);
+})
+
+
