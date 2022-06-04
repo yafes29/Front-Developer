ae461392d7a35bb30b15576507af2ca4928bad0b
mustafa <yafess229@gmail.com>
2022-06-04 22:15:55 +0300

    update

---
+++ JavaScript/divisible.js
@@ -0,0 +1,14 @@
+/*Question: Write a program which will find all such 
+numbers which are divisible by 7 but are not a multiple 
+of 5, between 2000 and 3200 (both included). The numbers 
+obtained should be printed in a comma-separated sequence 
+on a single line.*/
+
+liste=[];
+
+for(let i=2000;i<=3200;i++){
+    if (i%7==0 && i%5!=0){
+        liste.push(i);  
+    } 
+}
+ console.log(liste.join());
