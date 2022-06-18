6c44fe29504fff51d1aaa3072c741daa968712c1
mustafa <yafess229@gmail.com>
2022-06-18 12:26:29 +0300

    update

--- OOP/oop.js
+++ OOP/oop.js
@@ -65,3 +65,40 @@ function Book(title, author, year) {
   
   console.log(book1);
   console.log(book2.getSummary());
+
+  book1.price = 100;
+console.log(book1, book2);
+
+//! Eger yeni bir property veya fonksiyonu constructor'a eklemek istersek
+//! prototype'ı kullanabiliriz.
+
+Book.prototype.getAge = function () {
+  return new Date().getFullYear() - this.year;
+};
+
+Book.prototype.type = 'novel';
+
+console.log(book1.getAge());
+console.log(book2.getAge());
+console.log(book2.type);
+
+
+//! Prototype, belirli bir Nesne'nin (Object) tum instance'larina kolay bir
+//! sekilde metotlar tanimlamaya izin vermektedir.
+//! Prototip alaninda bir metot tanimlamanin guzelligi bu metot'un olusan
+//! tum instance'lerin belleginde yer kaplamamasi ancak tum instance'larin bu metota
+//! ulasabilmesidir.
+
+//* Ornegin Book nesnesinin tum instance'lari getAge() fonksiyonunu miras alabilir.
+//* Ancak, getAge() fonksiyonu bellekte sadece tek bir yer kaplamaktadir.
+
+//* Bir nesnenin prototiplerine .prototype ile erisilebilir.
+//* Ancak bir instance'in prototiplerine .__proto__ ile erisilmektedir.
+
+
+
+//!Object'lerin proto alanini gormk icin .protoype
+console.log(Book.prototype);
+
+//!instance'larin proto alanini gormk icin .__proto__
+console.log(book1.__proto__);
