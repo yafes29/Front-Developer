bd378362d79cfb3f754105e2c82b9622865d9ce9
mustafa <yafess229@gmail.com>
2022-06-22 16:30:45 +0300

    update

--- API/this/app.js
+++ API/this/app.js
@@ -1,3 +1,24 @@
-// console.log(this);
-// console.log(this.location)
-console.log(this.location.href)
+/*// THİS
+
+ console.log(this);
+console.log(this.location)
+ console.log(this.location.href)
+
+ console.log(this === window)
+
+ Function Declaration
+
+function func1(){
+    this.name = "ALP"
+    console.log(this)
+}
+
+ Function Expression
+
+const func2 = function(){
+    console.log(this)
+}
+
+console.log(name)
+func1();
+func2();*/
