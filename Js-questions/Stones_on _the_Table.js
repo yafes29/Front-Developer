793247d8dae87ad7f08644439e03352d8a0e80d2
mustafa <yafess229@gmail.com>
2022-06-24 13:05:37 +0300

    update

--- Js-questions/Stones_on _the_Table.js
+++ Js-questions/Stones_on _the_Table.js
@@ -1,21 +1,28 @@
 
 
-// function solve(stones) {
+//  function solve(stones) {
 
-//     let splıt = stones.split("")
-//     let count = 0;
-//     for (let i = 0; i < splıt.length; i++) {
+//      let splıt = stones.split("")
+     
+//      let count = 0;
+//      for (let i = 0; i < splıt.length; i++) {
 
 
-//         if (splıt[i] == splıt[i + 1]) {
-//             count += 1
+//          if (splıt[i] == splıt[i + 1]) {
+//              count += 1
 
-//         }
-//     }
-//     return count
-// }
+//          }
+//      }
+//      return count
+//  }
 
 // console.log(solve("RGGRGBBRGGBB"));
+
+
+
+
+
+
 
 
 
