6333d497f4629ddf99d9bbbfdbe97ed6018b438f
mustafa <yafess229@gmail.com>
2022-06-26 22:57:22 +0300

    update

--- API/this/app.js
+++ API/this/app.js
@@ -40,3 +40,18 @@ func2();*/
 //       return this;
 //     }
 //   };
+
+
+// const person1 = {
+//     fullName: function() {
+//       return this.firstName + " " + this.lastName;
+//     }
+//   }
+  
+//   const person2 = {
+//     firstName:"John",
+//     lastName: "Doe",
+//   }
+  
+//   // Return "John Doe":
+//   person1.fullName.call(person2);
