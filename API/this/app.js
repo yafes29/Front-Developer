73f0580ff7034166f0f426f73d9665439182bf93
mustafa <yafess229@gmail.com>
2022-06-27 21:17:13 +0300

    update

--- API/this/app.js
+++ API/this/app.js
@@ -57,8 +57,19 @@ func2();*/
 //   person1.fullName.call(person2);
 
 
-//  Regular Function:
+// // Regular Function:
 // hello = function() {
+//     document.getElementById("demo").innerHTML += this;
+//   }
+  
+//   // The window object calls the function:
+//   window.addEventListener("load", hello);
+  
+//   // A button object calls the function:
+//   document.getElementById("btn").addEventListener("click", hello);
+
+// // Arrow Function:
+// hello = () => {
 //     document.getElementById("demo").innerHTML += this;
 //   }
   
