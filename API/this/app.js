01969ccb108a8ed3e9b723211a4e3ff1fdcc2327
mustafa <yafess229@gmail.com>
2022-07-04 23:07:32 +0300

    update

--- API/this/app.js
+++ API/this/app.js
@@ -104,3 +104,30 @@ func2();*/
 //    //will return 28
 //    thisFunction.apply(object, [3, 6]);
 //    //will return 21
+
+
+// const person = {
+//     name : 'Jack',
+//     age: 25,
+
+//     // this inside method
+//     // this refers to the object itself
+//     greet() {
+//         console.log(this);        // {name: "Jack", age ...}
+//         console.log(this.age);  // 25
+
+//         // inner function
+//         function innerFunc() {
+        
+//             // this refers to the global object
+//             console.log(this);       // Window { ... }
+//             console.log(this.age);    // undefined
+            
+//         }
+
+//         innerFunc();
+
+//     }
+// }
+
+// person.greet();
